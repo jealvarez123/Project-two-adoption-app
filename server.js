f3764a7484cda8f79c0c5f59f24e6cf782e1ff33
jealvarez123 <jealvarez123@gmail.com>
2018-04-23 21:37:34 -0500

    created models, views, public folders. flushed up the server.js file with a route to the signup.ejs. created a seed.js file

---
+++ server.js
@@ -0,0 +1,31 @@
+console.log('Can I kick it?');
+//require express frameware and additional modules
+const express    = require('express');
+const app        = express();
+const bodyParser = require('body-parser');
+mongoose = require('mongoose');
+
+
+// middleware
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+ app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect('mongodb://@ds251799.mlab.com:51799/mlab');
+//// Routes ////
+
+// use res.render to load the ejs view file
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/signup', function (req, res) {
+  res.render('signup');
+});
+
+app.get('/login', function (req, res) {
+  res.send('login coming soon');
+});
+
+app.listen(3000, () => {
+  console.log('listening on 3000')
+})
