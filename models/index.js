2c20adc2f593de2bdc5efd568d555d7334022c0c
jealvarez123 <jealvarez123@gmail.com>
2018-04-25 12:39:02 -0500

    commented the exports.pet for right now

--- models/index.js
+++ models/index.js
@@ -2,4 +2,4 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/collection");
 
 module.exports.Pet = require("./pets.js");
-module.exports.Pet = require("./user.js");
+// module.exports.Pet = require("./user.js");
